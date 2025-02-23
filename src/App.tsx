ad814e95f9f7073dc9dcb1aa439e06373d93f1d4
Maher-alhomsy-se <maher.a@merkler00t.com>
2025-02-24 02:07:08 +0300

    fix eslint issue, add pay handler logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,66 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+import { ethers } from 'ethers';
+import { LoginButton } from '@telegram-auth/react';
 
 import './App.css';
 import connectWallet from './util/connectWallet';
-import { LoginButton } from '@telegram-auth/react';
+import { switchToBase } from './util/switchToBase';
+import { useAppKitAccount } from '@reown/appkit/react';
 
 function App() {
+  const { isConnected } = useAppKitAccount();
+
+  const [userId, setUserId] = useState<number | null>(null);
+
   useEffect(() => {
     connectWallet();
   }, []);
 
   const copyHandler = () => {
     navigator.clipboard.writeText('https://t.me/upfront_app');
+  };
+
+  const payHandler = async () => {
+    if (!window.ethereum || !isConnected) return;
+
+    // @ts-ignore
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const signer = await provider.getSigner();
+    const network = await provider.getNetwork();
+
+    if (network.chainId.toString() === '1301') {
+      console.log('transaction');
+
+      try {
+        const tx = await signer.sendTransaction({
+          to: '0x7562D34B4Cb64ff4A100d4e54f500b3d73C321Ce',
+          value: 1805000000000000,
+        });
+
+        const res = await tx.wait();
+        console.log('RES : ', res);
+        checkHandler(res!.hash);
+      } catch (error) {
+        console.log('ERROR', error);
+      }
+    } else {
+      await switchToBase();
+    }
+  };
+
+  const checkHandler = async (hash: string) => {
+    // @ts-ignore
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const tx = await provider.getTransaction(hash);
+    console.log(tx);
+
+    const v = ethers.formatEther(tx!.value);
+    console.log(v);
+
+    if (v === '0.001805') {
+      console.log('You pay 5$');
+    }
   };
 
   return (
@@ -39,8 +89,7 @@ function App() {
             buttonSize="large"
             botUsername={import.meta.env.VITE_BOT_USER_NAME}
             onAuthCallback={({ id }) => {
-              console.log(id);
-              // if (id) mutate({ address: address!, userId: id });
+              setUserId(id);
             }}
           />
         </span>
@@ -50,7 +99,10 @@ function App() {
         <span className="font-bold text-2xl">3.</span>
         <p className="text-xl">Pay 5$ to accept your joining request</p>
 
-        <span className="text-cyan-800 underline text-xl hover:text-cyan-900 cursor-pointer">
+        <span
+          onClick={isConnected && userId ? payHandler : connectWallet}
+          className="text-cyan-800 underline text-xl hover:text-cyan-900 cursor-pointer"
+        >
           Pay
         </span>
       </div>
