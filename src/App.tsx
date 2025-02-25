4a7f25a7eb924852a872646a407e790c5b26b2ae
Maher-alhomsy-se <maher.a@merkler00t.com>
2025-02-25 10:56:49 +0300

    check if there ethereum in window or not

--- src/App.tsx
+++ src/App.tsx
@@ -112,6 +112,7 @@ function App() {
   return (
     <main className="min-h-screen flex flex-col justify-center gap-6">
       {isConnected ? 'Connected' : 'Disconnected'}
+      {window.ethereum ? 'ethereum' : 'No ethereum'}
       <div className="flex items-center gap-2 flex-wrap">
         <div className="flex items-center gap-2">
           <span className="text-xl md:font-bold">1.</span>
